ca545c9a5e6d23c6d5eb0480d7dc507690b644e2
Daniel Malhadas <dmalhadas@hotmail.com>
2017-03-23 11:33:38 +0000

    Adiciona Servidor e Cliente

---
+++ Client.rb
@@ -0,0 +1,140 @@
+class Client < Object
+  require 'socket'  # Get sockets to send/receive data
+
+  require_relative 'Message' # Get message
+  require_relative 'menu'
+  require_relative 'Xdk'
+
+  def initialize (hostname = 'localhost', port = 2000, debug = 0)
+    @xdk      = Xdk.new
+    puts 'Write your ID'
+    @id       = STDIN.gets.chop
+    @debug    = debug
+    @hostname = hostname
+    @port     = port
+    puts('Connecting to Server')
+    @socket   = TCPSocket.open(hostname, port)
+    @open=1
+    main_menu
+  end
+
+  def main_menu
+    loop {
+      message = receive_message.split(';')
+      if message
+        puts('From: '+message[1]+', to: '+message[2])
+        case message[0]
+               when Message.HELLO
+                    puts(message[3].gsub!('\n',"\n"))
+                    STDIN.gets#.chop to take out new line
+                    send_message(Message.new(Message.REGISTER,
+                                             'Client',
+                                             'Server',
+                                             [@id.to_s]
+                                            )
+                                 ) # Send a Register Request
+
+               when Message.REGISTER
+                 case (message[3].include? 'OK')
+                   when true
+                     puts('Register Success')
+                     sending_menu
+                   when false
+                     puts('Register Failure. Closing.')
+                     @socket.close  # Close the socket when done
+                 end
+
+               when Message.LIST
+                 puts(message[3].gsub!('\n',"\n"))
+                 puts('Press ENTER to go back to the menu.')
+                 STDIN.gets#.chop to take out new line
+                 sending_menu
+
+               when Message.CLOSE
+                 @open=0
+                 puts(message[3].gsub!('\n',"\n"))
+                 puts('Press ENTER to terminate.')
+                 STDIN.gets#.chop to take out new line
+                 @socket.close  # Close the socket when done
+                 return
+
+             end
+      end
+    }
+  end
+
+  def sending_menu
+    puts(Menu.menu(1))
+    puts case STDIN.gets.chop
+           when '1'
+             send_message(Message.new(Message.LIST,
+                                      'Client',
+                                      'Server',
+                                      ['']
+                                      )
+                          ) # Send a List Request for a list of all clients
+           when '2'
+             puts('Please specify the ID of the Client: ')
+             option = STDIN.gets.chop
+             send_message(Message.new(Message.LIST,
+                                      'Client',
+                                      'Server',
+                                      [option.to_s]
+             )
+             ) # Send a List Request for readings on a specific client
+
+           when '3'
+             Thread.new {
+               simulate(Xdk.TEMPERATURE,'temperature',30)
+             }
+             sending_menu
+
+
+           when '4'
+             Thread.new {
+               simulate(Xdk.ACOUSTIC,'acoustic',1)
+             }
+             sending_menu
+
+           when '5'
+                send_message(Message.new(Message.CLOSE,
+                                         'Client',
+                                         'Server',
+                                         [@id.to_s]
+                                         )
+                              ) # Send a Close Request
+
+         end
+  end
+
+
+  def simulate(simulation_type, identifier, time)
+    while @open==1 do
+      puts 'Sending '+identifier if @debug==1
+      value = @xdk.simulate_new(simulation_type)
+      send_message(Message.new(Message.READING,
+                             'Client',
+                             'Server',
+                             [@id.to_s,identifier,value.to_s]
+                             )
+                 )
+      sleep(time) #wait for the specified seconds
+    end
+  end
+
+  def send_message(message)
+    @socket.puts(message.to_string) # Serialize Message and send it to Client
+    puts('Message Sent: '+message.to_string) if @debug==1
+  end
+
+  def receive_message
+    message = @socket.gets # Receive Message from client and deserialize it
+    puts('Message Received: '+message) if @debug==1
+    message
+  end
+
+
+end
+
+#client process
+Client.new()
---
+++ Server.rb
@@ -0,0 +1,115 @@
+class Server < Object
+  require 'socket' # Get sockets to send/receive data
+
+  require_relative 'Message' # Get message
+  require_relative 'menu'
+  require_relative 'Database' # Get database
+
+
+  def initialize(port=2000,debug=0)
+    @port     = port
+    @debug    = debug
+    @database = Database.new
+    server = TCPServer.open(@port) # Socket to listen on given port
+
+    loop do # Servers run forever
+      puts('Waiting for Clients')
+      Thread.fork(server.accept) do |client|
+        client_thread(client) # Method to handle each client/thread
+      end
+      puts('Client found')
+
+    end
+
+    #ensure
+     # @database.closeDB
+  end
+
+  def client_thread(client)
+
+    send_message(client, Message.new(Message.HELLO,
+                                     'Server',
+                                     'Client',
+                                     [Menu.menu(0)]
+                                    )
+                )
+    loop {
+      message = receive_message(client).split(';')
+      if message
+        puts('From: '+message[1]+', to: '+message[2])
+
+        case message[0]
+
+               when Message.REGISTER
+                 send_message(client, Message.new(Message.REGISTER,
+                                                  'Server',
+                                                  'Client',
+                                                  [@database.insert_or_replace_client(message[3].strip, get_current_time)]
+                              )
+                 ) # Send Register success or failure
+
+               when Message.LIST
+                 puts case message[3].strip
+                        when ''
+                          send_message(client, Message.new(Message.LIST,
+                                                           'Server',
+                                                           'Client',
+                                                           [@database.all_clients]
+                                                          )
+                                       ) # Send a Client list
+
+                        else
+                          send_message(client, Message.new(Message.LIST,
+                                                           'Server',
+                                                           'Client',
+                                                           [@database.client_readings(message[3].strip)]
+                                                           )
+                                        ) # Send a list of a Client's readings
+                      end
+
+               when Message.CLOSE
+                 puts('closing client '+client.to_s)
+                 send_message(client, Message.new(Message.CLOSE,
+                                                  'Server',
+                                                  'Client',
+                                                  [@database.last_readings(message[3].strip)]
+                                                  )
+                              ) # Send Closing remarks
+
+               when Message.READING
+                  res = message[3].strip.split('::')
+                  case res[1]
+                    when 'temperature'
+                      puts '--> Received Temperature reading from client #'+res[0]+', Value='+res[2]+'ºC'
+                      @database.insert_reading(res[0], get_current_time, res[2], 'temperature')
+                    when 'acoustic'
+                      puts '--> Received Acoustic reading from client #'+res[0]+', Value='+res[2]+'Db'
+                      @database.insert_reading(res[0], get_current_time, res[2], 'acoustic')
+                  end
+             end
+      end
+    }
+  end
+
+  def send_message(client, message)
+    client.puts(message.to_string) # send it to Client
+    puts('Message Sent: '+message.to_string) if @debug==1
+  end
+
+  def receive_message(client)
+    message = client.gets # Receive Message from client
+    puts('Message Received: '+message) if @debug==1
+    message
+  end
+
+  def get_current_time
+    date = Time.new
+    #get date in format -> YYYY-MM-DD HH:MM:SS.SSS
+    return date.year.to_s + '-' + date.month.to_s + '-' + date.day.to_s + ' ' + date.hour.to_s + ':' + date.min.to_s + ':' + date.sec.to_s + ':' + '000'
+  end
+
+end
+
+
+# Server process
+Server.new
